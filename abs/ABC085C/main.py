012eb950bf4ee700f4c9e7e0a718576a6b4c8a77
be.with <be.with.9078182@gmail.com>
2021-05-08 02:35:49 +0000

    ABC085C

--- abs/ABC085C/main.py
+++ abs/ABC085C/main.py
@@ -3,6 +3,15 @@ import sys
 
 
 def solve(N: int, Y: int):
+    for i in range(0,N+1):
+        for j in range(0, N+1):
+            k = N - i - j
+            if k < 0:
+                continue
+            if (i * 10000 + j * 5000 + k * 1000) == Y:
+                print(i,j,k)
+                return
+    print("-1 -1 -1")
     return
 
 
